83d0040edee4462ce0e65732a998b203af9b8add
yizhengzhou <yizheng@shuus-MacBook-Air.local>
2014-04-05 11:02:36 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RORProjects::Application.config.secret_key_base = 'e2a508e448802a07cee9000d00fa0a52bcdcbf652e9bae3a6aef0dbfe6be5da114ff89ff39f34873a55c1d5fc0d5660e73bbb82c0c61b4b3fb2b79d878218f8b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RORProjects::Application.config.session_store :cookie_store, key: '_ROR_projects_session'
